301ecc6a52b121fc97f5383a9ac6964495da1218
zand <luthfisandi20@gmail.com>
2021-09-01 13:21:45 +0700

    add db env for production

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-let db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-})
+let db;
+if (process.env.NODE_ENV === 'development') {
+  db = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+  })
+} else if (process.env.NODE_ENV === 'production') {
+
+  db = new Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+
 
 module.exports = db;
